341a51a4912cba07ac3fa8d14d078a8702fb0576
silnex <silnex@silnex.kr>
2024-08-06 23:50:01 +0800

    Impl interceptor

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,32 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  UseInterceptors,
+} from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { map, Observable } from 'rxjs';
+
+interface ClassConstructor {
+  new (...args: any[]): object;
+}
+
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+@Injectable()
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private readonly dto: any) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data: any) => {
+        return plainToClass(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
