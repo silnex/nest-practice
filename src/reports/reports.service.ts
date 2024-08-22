094611448f0955ebf7ac6e0c532d8870df79a33a
silnex <silnex@silnex.kr>
2024-08-22 23:52:57 +0800

    Add realtion User to Report

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Report } from './report.entity';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { User } from '../users/user.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(
+    @InjectRepository(Report) private readonly repo: Repository<Report>,
+  ) {}
+
+  create(createReportDto: CreateReportDto, user: User) {
+    const report = this.repo.create(createReportDto);
+    report.user = user;
+
+    return this.repo.save(report);
+  }
+}
